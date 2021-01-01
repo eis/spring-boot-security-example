bb2699c0458a1a41a540ffa0104f515094ec896c
eis <eis@iki.fi>
2021-01-01 22:20:54 +0200

    Spring Boot 2.4.1 update: compile time changes

--- src/main/java/example/hello/HelloSecurityConfig.java
+++ src/main/java/example/hello/HelloSecurityConfig.java
@@ -1,25 +1,24 @@
 package example.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static example.hello.HelloSecurityRoles.ADMIN;
+import static example.hello.HelloSecurityRoles.USER;
+
 @Configuration
 @EnableWebSecurity
-public class HelloConfig extends WebSecurityConfigurerAdapter {
-    static class Roles {
-        static final String ADMIN = "ADMIN";
-        static final String USER = "USER";
-    }
+public class HelloSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                     .antMatchers("/login", "/webjars/**", "/css/**", "/js/**").permitAll()
-                    .antMatchers("/admin").hasRole(Roles.ADMIN)
+                    .antMatchers("/admin").hasRole(ADMIN)
                     .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -29,12 +28,7 @@ public class HelloConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("admin").password("123456").roles(Roles.ADMIN)
-            .and().withUser("user").password( "password").roles(Roles.USER);
+            .withUser("admin").password("123456").roles(ADMIN)
+            .and().withUser("user").password( "password").roles(USER);
     }
-    @Bean
-    public HelloMVCController helloMVCController() {
-        return new HelloMVCController();
-    }
-
 }
---
+++ src/main/java/example/hello/HelloSecurityRoles.java
@@ -0,0 +1,6 @@
+package example.hello;
+
+public class HelloSecurityRoles {
+    static final String ADMIN = "ADMIN";
+    static final String USER = "USER";
+}
