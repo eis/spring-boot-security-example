fc6c7fe19a0d80f7a4df302941ce11abd875642b
eis <eis@iki.fi>
2021-01-01 16:46:07 +0200

    Always redirect to login page

--- src/main/java/example/hello/HelloConfig.java
+++ src/main/java/example/hello/HelloConfig.java
@@ -3,14 +3,10 @@ package example.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +21,6 @@ public class HelloConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/login", "/webjars/**", "/css/**", "/js/**").permitAll()
                     .antMatchers("/admin").hasRole(Roles.ADMIN)
                     .anyRequest().authenticated()
-                .and()
-                    .exceptionHandling()
-                    .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.NOT_FOUND),
-                            new NegatedRequestMatcher(new AntPathRequestMatcher("/login**")) )
                 .and()
                 .formLogin()
                 ;
