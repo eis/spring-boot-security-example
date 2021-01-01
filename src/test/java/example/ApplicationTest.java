dd407cad253b67f6173064db8eff85f79d7f4fe1
eis <eis@iki.fi>
2021-01-01 17:16:30 +0200

    Improved test a bit

--- src/test/java/example/ApplicationTest.java
+++ src/test/java/example/ApplicationTest.java
@@ -4,14 +4,17 @@ import example.hello.HelloMVCController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
     @Autowired
     ApplicationContext applicationContext;
@@ -20,5 +23,17 @@ public class ApplicationTest {
     public void contextLoads() throws Exception {
         assertNotNull(applicationContext);
         assertNotNull(applicationContext.getBean(HelloMVCController.class));
+    }
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loginShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/login",
+                String.class)).contains("Username").contains("Password");
     }
 }
