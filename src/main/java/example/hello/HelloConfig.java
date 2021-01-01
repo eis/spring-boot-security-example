e8cded6501f3dc00ce94e1d9b66813ad89ade8b1
eis <eis@iki.fi>
2021-01-01 16:43:31 +0200

    Two different user roles

--- src/main/java/example/hello/HelloConfig.java
+++ src/main/java/example/hello/HelloConfig.java
@@ -15,10 +15,15 @@ import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class HelloConfig extends WebSecurityConfigurerAdapter {
+    static class Roles {
+        static final String ADMIN = "ADMIN";
+        static final String USER = "USER";
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                     .antMatchers("/login", "/webjars/**", "/css/**", "/js/**").permitAll()
+                    .antMatchers("/admin").hasRole(Roles.ADMIN)
                     .anyRequest().authenticated()
                 .and()
                     .exceptionHandling()
@@ -31,7 +36,9 @@ public class HelloConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+        auth.inMemoryAuthentication()
+            .withUser("admin").password("123456").roles(Roles.ADMIN)
+            .and().withUser("user").password( "password").roles(Roles.USER);
     }
     @Bean
     public HelloMVCController helloMVCController() {
